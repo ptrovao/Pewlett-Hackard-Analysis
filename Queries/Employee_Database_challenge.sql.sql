ab014723b67a7a035f2f9db120d6f7f807a54ac3
ptrovao <93052605+ptrovao@users.noreply.github.com>
2021-12-12 16:21:32 -0500

    Update Employee_Database_challenge.sql.sql

--- Queries/Employee_Database_challenge.sql.sql
+++ Queries/Employee_Database_challenge.sql.sql
@@ -8,10 +8,7 @@ WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 order by e.emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-title
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
 INTO uniq_titles
 FROM retirement_titles
 ORDER BY emp_no, title DESC;
@@ -22,3 +19,14 @@ INTO retiring_titles
 FROM uniq_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON(e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibilty
+FROM employees as e
+LEFT OUTER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT OUTER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
